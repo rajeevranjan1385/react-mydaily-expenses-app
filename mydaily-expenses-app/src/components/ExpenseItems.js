da0e4a6e2082018e7610703a1c150a58c964cf8b
Rupal <rupalrawat067@gmail.com>
2021-08-11 22:19:59 +0530

    few more changes on app.js

---
+++ mydaily-expenses-app/src/components/ExpenseItems.js
@@ -0,0 +1,14 @@
+function ExpenseItem(){
+    return (
+        <div>
+            <ul>
+                <li>Washing Soap</li>
+                <li>Cloathes</li>
+                <li>Sandle</li>
+                <li>Rain Coat</li>
+            </ul>
+        </div>
+    )
+}
+
+export default ExpenseItem
