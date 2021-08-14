4fc9b2ab37cbce00dcf3756dfa7e7780fe8f565c
Rupal <rupalrawat067@gmail.com>
2021-08-14 23:39:27 +0530

    changes on 14th Aug

--- mydaily-expenses-app/src/components/ExpenseItems.js
+++ mydaily-expenses-app/src/components/ExpenseItems.js
@@ -1,15 +1,19 @@
-import './ExpenseItems.css'
+import './ExpenseItems.css';
 
 function ExpenseItem(){
-    return (
-        <div className="expense-item">
-            <div>Audi New </div>
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 300000;
+    const expenseDate = new Date(2021, 7, 14);
+
+    return(
+        <div className='expense-item'>
+            <div>{expenseDate.toISOString()}</div>
             <div className='expense-item__description'>
-                <h2>Car</h2>
-                <div className='expense-item__price'>Rs 64,00,000/-</div>
+                <h2>{expenseTitle}</h2>
+                <div className='expense-item__price'>Rs {expenseAmount}/-</div>
             </div>
         </div>
     )
 }
 
-export default ExpenseItem
+export default ExpenseItem;
