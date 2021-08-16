210a144e31577c47ee399502f96073f3cdbf8109
Rupal <rupalrawat067@gmail.com>
2021-08-16 07:55:36 +0530

    formatting date

--- mydaily-expenses-app/src/components/ExpenseItems.js
+++ mydaily-expenses-app/src/components/ExpenseItems.js
@@ -1,16 +1,20 @@
 import './ExpenseItems.css';
 
-function ExpenseItem(){
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = 300000;
-    const expenseDate = new Date(2021, 7, 14);
+function ExpenseItem(props){
+    const month = props.date.toLocaleString('en-US', { month: 'long'});
+    const day = props.date.toLocaleString('en-US', { day: '2-digit'});
+    const year = props.date.getFullYear();
 
     return(
         <div className='expense-item'>
-            <div>{expenseDate.toISOString()}</div>
+            <div>
+                <div>{month}</div>
+                <div>{year}</div>
+                <div>{day}</div>
+            </div>
             <div className='expense-item__description'>
-                <h2>{expenseTitle}</h2>
-                <div className='expense-item__price'>Rs {expenseAmount}/-</div>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>Rs {props.amount}/-</div>
             </div>
         </div>
     )
