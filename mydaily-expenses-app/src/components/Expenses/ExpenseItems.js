c89b33c2de46c925a14e52b7a0dcb9d076d18337
rajeevranjan1385 <rajeevranjan1385@gmail.com>
2021-08-18 07:52:26 +0530

    added a handler

--- mydaily-expenses-app/src/components/Expenses/ExpenseItems.js
+++ mydaily-expenses-app/src/components/Expenses/ExpenseItems.js
@@ -4,12 +4,17 @@ import ExpenseDate from './ExpenseDate';
 import './ExpenseItems.css';
 
 const ExpenseItems = (props) => {
+    const title = props.title;
+    const clickHandler = () => {
+        title = 'Updated!';
+    }
     return(
         <Card className='expense-item'>
             <ExpenseDate date={props.date} />
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>{props.amount}</div>
+                <button onClick={clickHandler}>Change Title</button>
             </div>
         </Card>
     );
