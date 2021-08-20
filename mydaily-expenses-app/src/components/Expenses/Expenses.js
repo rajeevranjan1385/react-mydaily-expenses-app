57e0de507fc0736bb1c8a72f63b32f40f873f75d
rajeevranjan1385 <rajeevranjan1385@gmail.com>
2021-08-20 07:56:34 +0530

    assignment 2

--- mydaily-expenses-app/src/components/Expenses/Expenses.js
+++ mydaily-expenses-app/src/components/Expenses/Expenses.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Expenses.css';
 import ExpenseItem from './ExpenseItems';
 import Card from '../UI/Card';
+import ExpenseFilter from './ExpenseFilter';
 
 const Expenses = (props) => {
+    const [filteredYear, setFilteredYear] = useState('2019');
+    const SelectedYearValue = (value) => {
+        setFilteredYear(value);
+        console.log('The selected year is ' + value);
+    }
+
     return(
         <Card className='expenses'>
+            <ExpenseFilter selectedYear={SelectedYearValue} filteredYearValue={filteredYear} />
             <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date}></ExpenseItem>
             <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date}></ExpenseItem>
             <ExpenseItem title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date}></ExpenseItem>
