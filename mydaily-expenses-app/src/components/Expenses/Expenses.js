869e2e8dcd971eeed1e3dde94d48e78965f0bd1b
rajeevranjan1385 <rajeevranjan1385@gmail.com>
2021-08-21 07:36:50 +0530

    added ExpenseList component

--- mydaily-expenses-app/src/components/Expenses/Expenses.js
+++ mydaily-expenses-app/src/components/Expenses/Expenses.js
@@ -1,25 +1,19 @@
 import React, {useState} from 'react';
 import './Expenses.css';
-import ExpenseItem from './ExpenseItems';
 import Card from '../UI/Card';
 import ExpenseFilter from './ExpenseFilter';
+import ExpenseList from './ExpenseList';
 
 const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState('2019');
     const SelectedYearValue = (value) => {
         setFilteredYear(value);
     }
-    let expensesContent = <p>There are no rows to display</p>;
     const filteredExpenses = props.items.filter(year => year.date.getFullYear() == filteredYear);
-    if(filteredExpenses.length > 0){
-        expensesContent = filteredExpenses.map(expense => 
-            (<ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />))
-    }
-
     return(
         <Card className='expenses'>
             <ExpenseFilter selectedYear={SelectedYearValue} filteredYearValue={filteredYear} />
-            {expensesContent}
+            <ExpenseList items={filteredExpenses} />
         </Card>
     );
 }
