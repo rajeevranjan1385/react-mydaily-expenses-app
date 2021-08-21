53eeba3678e3bd612ed56a8611723cdd50d8672a
rajeevranjan1385 <rajeevranjan1385@gmail.com>
2021-08-21 08:38:38 +0530

    assignment 4 completed

--- mydaily-expenses-app/src/components/CreateExpense/CreateExpense.js
+++ mydaily-expenses-app/src/components/CreateExpense/CreateExpense.js
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './CreateExpense.css';
 import ExpenseForm from './ExpenseForm';
 
 const CreateExpense = (props) => {
+    const [showExpenseForm, setShowExpenseForm] = useState(false)
     const saveEnteredExpenseData = (enteredExpenseData) => {
         const expenseData = {
             ...enteredExpenseData,
             id: Math.random().toString()
         }
         props.onAddExpense(expenseData);
+        setShowExpenseForm(false);
+    }
+    const showHideExpenseForm = () => {
+        setShowExpenseForm(true);
+    }
+
+    const AddNewExpenseHandler = () =>{
+        showHideExpenseForm();
+    }
+
+    const CancelButtonHandler = () => {
+        setShowExpenseForm(false);
     }
     return(
         <div className='new-expense'>
-            <ExpenseForm onSaveExpenseData={saveEnteredExpenseData} />
+            {!showExpenseForm && <button onClick={AddNewExpenseHandler}>Add New Expense</button>}
+            {showExpenseForm && <ExpenseForm onSaveExpenseData={saveEnteredExpenseData} onCancelButtonClick={CancelButtonHandler} />}
         </div>
     )
 }
