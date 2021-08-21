5cf5cd836dd92e7d86a85e03e8cfb5a36a8fe3ff
rajeevranjan1385 <rajeevranjan1385@gmail.com>
2021-08-21 19:37:53 +0530

    adding chart

--- mydaily-expenses-app/src/components/Expenses/Expenses.js
+++ mydaily-expenses-app/src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import './Expenses.css';
 import Card from '../UI/Card';
 import ExpenseFilter from './ExpenseFilter';
 import ExpenseList from './ExpenseList';
+import ExpenseChart from './ExpenseChart';
 
 const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState('2019');
@@ -11,10 +12,13 @@ const Expenses = (props) => {
     }
     const filteredExpenses = props.items.filter(year => year.date.getFullYear() == filteredYear);
     return(
-        <Card className='expenses'>
-            <ExpenseFilter selectedYear={SelectedYearValue} filteredYearValue={filteredYear} />
-            <ExpenseList items={filteredExpenses} />
-        </Card>
+        <div>
+            <Card className='expenses'>
+                <ExpenseFilter selectedYear={SelectedYearValue} filteredYearValue={filteredYear} />
+                <ExpenseChart expenses={filteredExpenses}/>
+                <ExpenseList items={filteredExpenses} />
+            </Card>
+        </div>
     );
 }
 
