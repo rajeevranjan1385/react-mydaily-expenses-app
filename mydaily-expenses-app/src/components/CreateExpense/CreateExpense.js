f8ad0726e4f024d4f6807bb818651bab1eaa5019
rajeevranjan1385 <rajeevranjan1385@gmail.com>
2021-08-21 22:06:18 +0530

    formatted code using prettier

--- mydaily-expenses-app/src/components/CreateExpense/CreateExpense.js
+++ mydaily-expenses-app/src/components/CreateExpense/CreateExpense.js
@@ -1,34 +1,41 @@
-import React, { useState } from 'react';
-import './CreateExpense.css';
-import ExpenseForm from './ExpenseForm';
+import React, { useState } from "react";
+import "./CreateExpense.css";
+import ExpenseForm from "./ExpenseForm";
 
 const CreateExpense = (props) => {
-    const [showExpenseForm, setShowExpenseForm] = useState(false)
-    const saveEnteredExpenseData = (enteredExpenseData) => {
-        const expenseData = {
-            ...enteredExpenseData,
-            id: Math.random().toString()
-        }
-        props.onAddExpense(expenseData);
-        setShowExpenseForm(false);
-    }
-    const showHideExpenseForm = () => {
-        setShowExpenseForm(true);
-    }
+  const [showExpenseForm, setShowExpenseForm] = useState(false);
+  const saveEnteredExpenseData = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+    props.onAddExpense(expenseData);
+    setShowExpenseForm(false);
+  };
+  const showHideExpenseForm = () => {
+    setShowExpenseForm(true);
+  };
 
-    const AddNewExpenseHandler = () =>{
-        showHideExpenseForm();
-    }
+  const AddNewExpenseHandler = () => {
+    showHideExpenseForm();
+  };
 
-    const CancelButtonHandler = () => {
-        setShowExpenseForm(false);
-    }
-    return(
-        <div className='new-expense'>
-            {!showExpenseForm && <button onClick={AddNewExpenseHandler}>Add New Expense</button>}
-            {showExpenseForm && <ExpenseForm onSaveExpenseData={saveEnteredExpenseData} onCancelButtonClick={CancelButtonHandler} />}
-        </div>
-    )
-}
+  const CancelButtonHandler = () => {
+    setShowExpenseForm(false);
+  };
+  return (
+    <div className="new-expense">
+      {!showExpenseForm && (
+        <button onClick={AddNewExpenseHandler}>Add New Expense</button>
+      )}
+      {showExpenseForm && (
+        <ExpenseForm
+          onSaveExpenseData={saveEnteredExpenseData}
+          onCancelButtonClick={CancelButtonHandler}
+        />
+      )}
+    </div>
+  );
+};
 
 export default CreateExpense;
--- mydaily-expenses-app/src/components/Expenses/Expenses.js
+++ mydaily-expenses-app/src/components/Expenses/Expenses.js
@@ -1,25 +1,30 @@
-import React, {useState} from 'react';
-import './Expenses.css';
-import Card from '../UI/Card';
-import ExpenseFilter from './ExpenseFilter';
-import ExpenseList from './ExpenseList';
-import ExpenseChart from './ExpenseChart';
+import React, { useState } from "react";
+import "./Expenses.css";
+import Card from "../UI/Card";
+import ExpenseFilter from "./ExpenseFilter";
+import ExpenseList from "./ExpenseList";
+import ExpenseChart from "./ExpenseChart";
 
 const Expenses = (props) => {
-    const [filteredYear, setFilteredYear] = useState('2019');
-    const SelectedYearValue = (value) => {
-        setFilteredYear(value);
-    }
-    const filteredExpenses = props.items.filter(year => year.date.getFullYear() == filteredYear);
-    return(
-        <div>
-            <Card className='expenses'>
-                <ExpenseFilter selectedYear={SelectedYearValue} filteredYearValue={filteredYear} />
-                <ExpenseChart expenses={filteredExpenses}/>
-                <ExpenseList items={filteredExpenses} />
-            </Card>
-        </div>
-    );
-}
+  const [filteredYear, setFilteredYear] = useState("2019");
+  const SelectedYearValue = (value) => {
+    setFilteredYear(value);
+  };
+  const filteredExpenses = props.items.filter(
+    (year) => year.date.getFullYear() == filteredYear
+  );
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpenseFilter
+          selectedYear={SelectedYearValue}
+          filteredYearValue={filteredYear}
+        />
+        <ExpenseChart expenses={filteredExpenses} />
+        <ExpenseList items={filteredExpenses} />
+      </Card>
+    </div>
+  );
+};
 
 export default Expenses;
